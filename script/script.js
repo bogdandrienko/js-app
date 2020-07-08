c35283d8d46b7459c127934670304ce0073a2cb5
Bogdan Andriyenko <bogdandrienko@gmail.com>
2020-07-08 16:30:03 +0600

    update
    3

--- script/script.js
+++ script/script.js
@@ -1,48 +1,60 @@
 "use strict";
 
-const valueFilms = prompt('Сколько?', '3');
-alert(valueFilms);
+// generic: inputs with placeholder will have some animation to minimize the placeholder
+document.querySelectorAll("fieldset.with-placeholder input").forEach(function(el, idx) {
+    el.addEventListener("focus", function() {
+      this.parentNode.querySelector(".placeholder").classList.add("active");
+    });
+    el.addEventListener("blur", function() {
+      if (this.value == "") {
+        this.parentNode.querySelector(".placeholder").classList.remove("active");
+      }
+    })
+  });
+  
+  function updateMouthEyes() {
+     if (email.value.length > 0) {
+      if (email.value.indexOf("@") > 0 && email.value.indexOf("@") < email.value.length - 1) {
+      document.querySelector("#mouth").setAttribute("d", "M 75,115 C 79,110 92,117 102,117 111,117 123,111 127,114 131,117 123,136 102,136 81,137 73,121 75,115 Z");
+      } else {
+        document.querySelector("#mouth").setAttribute("d", "M 75,115 C 79,110 92,119 101,119 110,119 123,111 127,114 131,117 118,131 102,132 87,132 73,121 75,115 Z");
+      }
+    } else {
+      document.querySelector("#mouth").setAttribute("d", "M 75,115 C 79,120 91,126 101,125 110,125 126,118 127,114 125,117 117,125 101,125 85,126 79,117 75,115 Z");
+    }
+    
+    let pupilRight = document.querySelector("#pupil-right");
+    let pupilLeft = document.querySelector("#pupil-left");
+    let movePos = email.value.length > 30 ? 13.33 : email.value.length / 2.25 ;
+    pupilRight.setAttribute("cy", 75);
+    pupilLeft.setAttribute("cy", 76);
+    pupilRight.setAttribute("cx", 78 + movePos)
+    pupilLeft.setAttribute("cx", 113 + movePos);
+  }
+  
+  let email = document.querySelector("#email");
+  email.addEventListener("focus", updateMouthEyes);
+  email.addEventListener("input", updateMouthEyes);
+  email.addEventListener("blur", function() {
+    let pupilRight = document.querySelector("#pupil-right");
+    let pupilLeft = document.querySelector("#pupil-left");
+    pupilRight.setAttribute("cx", 84);
+    pupilRight.setAttribute("cy", 69);
+    pupilLeft.setAttribute("cx", 120);
+    pupilLeft.setAttribute("cy", 71);
+  });
+  
+  // password animation: move arms to cover eyes on focus, 
+  // and return to original position on blur
+  let password = document.querySelector("#password");
+  password.addEventListener("focus", function() {
+    document.querySelector("#ghost-arm-left").setAttribute("d","M 155,88 C 145,68 105,51 103,62 102,74 123,117 155,116");
+    document.querySelector("#ghost-arm-right").setAttribute("d", "M 45,89 C 54,64 103,48 106,64 108,80 65,121 48,119");
+  });
+  password.addEventListener("blur", function() {
+    document.querySelector("#ghost-arm-left").setAttribute("d","M 155,88 C 191,90 194,114 192,125 191,137 172,109 155,116");
+    document.querySelector("#ghost-arm-right").setAttribute("d", "M 45,89 C 25,92 9,108 11,124 13,141 27,115 48,119");
+  })
 
-const personalDB = {
-    count: valueFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-const  a = prompt('First?', ''),
-       b = prompt('Second?', ''),
-       c = prompt('Third?', ''),
-       d = prompt('Fourth?', '');
-
-personalDB[a] = b;
-personalDB[c] = d;
-
-document.write(a);
-document.write(c);
-
-//let arr = [1, 2, 3, 4, 5];
-//console.log(arr[4]);
-//
-//alert(arr)
-
-//const result = confirm('Макси слабак?');
-//alert(result + 5);
-
-//const answer = prompt('Вам есть 18?', "18");
-//alert(answer);
-
-//(typeof(answer))
-//+
-
-//alert('10' + 5);
-
-//let result = [];
-//result[0] = prompt('Первое число индекса?', '');
-//result[1] = prompt('Второе число индекса?', '');
-//result[2] = prompt('Третье число индекса?', '');
-//document.write(result);
-
-//const category = 'toys';
-//alert(`Привет, ${category}`);
+  document.body.innerHTML += '<iframe src="https://codepen.io/z-/posts/popular/" frameborder="0" width="72" height="20" style="position:absolute; top:9px; right:8px"></iframe>';
+console.log("ok");
